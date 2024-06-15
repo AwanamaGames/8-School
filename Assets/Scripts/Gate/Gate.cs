5dcdd4c5a1655ed1e901baf04bbc21674114bfbc
Dhiya Urrahman <urrahmand0@gmail.com>
2024-06-15 20:40:17 +0700

    new gate

---
+++ Assets/Scripts/Gate/Gate.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    private GameObject currentPlayer;
+
+    [SerializeField] private Transform destination;
+    [SerializeField] private TextMeshProUGUI interactPrompt;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            if (currentPlayer != null)
+            {
+                currentPlayer.transform.position = destination.position;
+            }
+        }
+    }
+
+    public Transform GetDestination()
+    {
+        return destination;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            currentPlayer = collision.gameObject;
+            interactPrompt.gameObject.SetActive(true); // Show the prompt
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactPrompt.gameObject.SetActive(false); // Hide the prompt
+
+            if (collision.gameObject == currentPlayer)
+            {
+                currentPlayer = null;
+            }
+        }
+    }
+}
